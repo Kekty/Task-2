425b9c64ab17879051f0d0265d7a590b2eed90ff
Lisa <kektisheva.elizaveta@gmail.com>
2020-02-23 22:22:24 +0700

    added button

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 // import "./assets/css/main.css";
 import "./scss/main.scss";
 
+// import "./components/logo/logo.scss";
+// import "./components/button/button.scss";
+
 //import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 //import 'bootstrap/dist/css/bootstrap.min.css'
-
-
-
